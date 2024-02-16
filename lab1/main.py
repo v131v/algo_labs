ad3624e7660cf3cd2f67104450776ccda1b40178
v131v <liya.dobrenkova@ya.ru>
2024-02-16 23:58:40 +0300

    docs: add lab1 report

--- lab1/main.py
+++ lab1/main.py
@@ -3,31 +3,28 @@ from compute import *
 from tkinter import messagebox
 
 if __name__ == "__main__":
-    
-    # Создание основного окна
+
     root = tk.Tk()
     root.title("Dataset generator")
 
     output_frame = tk.Frame(root)
     output_frame.pack(side=tk.TOP, padx=10)
-    
+
     output_label = tk.Label(output_frame, text="Output filename")
     output_label.grid(row=0, column=0, padx=5, pady=5, sticky=tk.E)
 
     output_entry = tk.Entry(output_frame)
     output_entry.grid(row=0, column=1, padx=5, pady=5, sticky=tk.W)
-    
-    
+
     compute_frame = tk.Frame(root)
     compute_frame.pack(side=tk.BOTTOM, padx=10)
-    
+
     count_label = tk.Label(compute_frame, text="Count")
     count_label.grid(row=0, column=0, padx=5, pady=5, sticky=tk.E)
 
     count_entry = tk.Entry(compute_frame)
     count_entry.grid(row=0, column=1, padx=5, pady=5, sticky=tk.W)
 
-    # Первый столбец
     column1_frame = tk.Frame(root)
     column1_frame.pack(side=tk.LEFT, padx=10)
 
@@ -51,7 +48,6 @@ if __name__ == "__main__":
     d_entry = tk.Entry(column1_frame)
     d_entry.grid(row=3, column=1, padx=5, pady=5)
 
-    # Второй столбец
     column2_frame = tk.Frame(root)
     column2_frame.pack(side=tk.LEFT, padx=10)
 
@@ -69,7 +65,7 @@ if __name__ == "__main__":
     z_label.grid(row=2, column=0, padx=5, pady=5, sticky=tk.E)
     z_entry = tk.Entry(column2_frame)
     z_entry.grid(row=2, column=1, padx=5, pady=5)
-    
+
     def handler():
         banks_p = {
             "sberbank": int(a_entry.get()),
@@ -83,7 +79,7 @@ if __name__ == "__main__":
         if s != 100:
             messagebox.showinfo("Error", "Sum of banks probabilities != 100")
             return
-        
+
         systems_p = {
             "mastercard": int(x_entry.get()),
             "visa": int(y_entry.get()),
@@ -95,14 +91,11 @@ if __name__ == "__main__":
         if s != 100:
             messagebox.showinfo("Error", "Sum of payment systems probabilities != 100")
             return
-        
+
         compute(int(count_entry.get()), output_entry.get(), banks_p, systems_p)
         messagebox.showinfo("Success", "Dataset saved")
-    
-    # Кнопка для выполнения операции (вашего выбора)
+
     calculate_button = tk.Button(compute_frame, text="Compute", command=handler)
     calculate_button.grid(row=2, columnspan=2, pady=10)
 
-    # Запуск цикла обработки событий
     root.mainloop()
-        
